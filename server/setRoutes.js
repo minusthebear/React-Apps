e777d68a921773e132fcbe29f60633d8f1f7a59e
matthewhamann <matt.hamann1982@gmail.com>
2019-06-05 17:27:50 -0700

    Test.html rendering worked

--- server/setRoutes.js
+++ server/setRoutes.js
@@ -6,8 +6,10 @@ const setRoutes = app => {
 
 	app.get('/test', function(req, res) {
 		console.log('IT HIT TEST');
-		console.log(path.join('..', __dirname, 'public', 'index.html'));
-		res.sendFile(path.join('..', __dirname, 'public', 'index.html'));
+		console.log(path.join());
+		console.log(path.resolve('public', 'index.html'));
+		// console.log(path.join('..', __dirname, 'public', 'index.html'));
+		res.sendFile(path.resolve('public', 'test.html'));
 	});
 
 	app.get('/*', function(req, res)  {
