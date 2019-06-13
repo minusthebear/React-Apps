c9977211bc85f905b95b90ead6b22194f1104575
Matthew Hamann <matt.hamann1982@gmail.com>
2019-06-14 05:52:21 +0900

    Here is a test commit from a new computer

--- public/index.jsx
+++ public/index.jsx
@@ -16,6 +16,8 @@ render(
     document.getElementById('root')
 );
 
+// This is a comment for a test commit
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
