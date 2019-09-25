3eb0e17548883f2c934aede4610ecbd111203986
minusthebear <matt.hamann1982@gmail.com>
2019-09-25 10:52:30 -0700

    Removing unnecessary console.logs

--- server/routes/sessionChecker.js
+++ server/routes/sessionChecker.js
@@ -1,9 +1,6 @@
 const authenticationTokens = require('../authenticationTokens');
 
 function sessionChecker(req, res, next) {
-    console.log('SessionChecker');
-    console.log(req.session.user);
-
     if (req.session.user && req.session.user.token) {
         next();
     } else {
