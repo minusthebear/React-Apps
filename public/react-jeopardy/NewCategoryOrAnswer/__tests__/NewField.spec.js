8553e25f39184d9e3cf1ad4a8cc5912608c98d3e
minusthebear <matt.hamann1982@gmail.com>
2019-10-03 16:06:44 -0700

    Enzyme smoke test

---
+++ public/react-jeopardy/NewCategoryOrAnswer/__tests__/NewField.spec.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import NewField from '../NewField';
+
+
+describe("NewField",function() {
+    let newField;
+
+
+    //
+    // it('smoke test', () => {
+    //     let mountedNewField = shallow(<NewField />);
+    // });
+
+    //
+    // it('renders a button', () => {
+    //     const button = mountedButton.find('button');
+    //     expect(button.length).toBe(1);
+    // });
+});
